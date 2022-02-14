d9103ee2edd94dd53fe12740023a3cc31a1b4eab
SukWooJung <ggp03016@naver.com>
2022-02-14 15:56:57 +0900

    refact: position 필드를 private set 형태로 변경 후 처리 (접근제어자 고민 이휴 해결) feat: 이름을 출력하는 printName 함수 생성

--- src/main/kotlin/ornn/racingcar/data/Car.kt
+++ src/main/kotlin/ornn/racingcar/data/Car.kt
@@ -1,23 +1,33 @@
 package ornn.racingcar.data
 
 import ornn.racingcar.Application
-import java.util.Random
 
-data class Car (private val name : String){
-    private var position: Int = 0
+data class Car(private val name: String) {
+
+    var position: Int = 0
+        private set
 
     fun printRecord() {
         print("${name} : ")
-        for (i in 1..position){
+        for (i in 1..position) {
             print("-")
         }
         println()
     }
 
-    fun move(){
-        val num = Application.random.nextInt(10)
-        if (num >= 4) {
+    fun move() {
+        val randomNum = Application.random.nextInt(MOVE_TO - MOVE_FROM) + MOVE_FROM
+        if (randomNum >= 4) {
             position++
         }
+    }
+
+    fun printName(){
+        print(name)
+    }
+
+    companion object {
+        const val MOVE_FROM = 0
+        const val MOVE_TO = 10
     }
 }
