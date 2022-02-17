c53264c173f92922433ba62a2256fd2307555d22
osandra <okhw97@gmail.com>
2022-02-17 22:44:44 +0900

    refactor: 유효하지 않은 값을 입력받을 때 예외를 던지는 방향으로 수정

--- src/main/kotlin/water/racingcar/ui/GameInputView.kt
+++ src/main/kotlin/water/racingcar/ui/GameInputView.kt
@@ -1,56 +1,65 @@
 package water.racingcar.ui
 
-import kotlin.math.abs
+import water.racingcar.exception.EmptyCarNamesInputException
+import water.racingcar.exception.InvalidCarNamesLengthInputException
+import water.racingcar.exception.InvalidInputRacingCarException
+import water.racingcar.exception.InvalidTimesInputException
 
 object GameInputView {
     private const val CAR_NAME_INPUT_INFO_MESSAGE = "경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분) "
     private const val CAR_NAME_INPUT_ERROR_INFO_MESSAGE = "※ 이름은 5자 이하만 가능합니다."
+    private const val EMPTY_CAR_NAME_ERROR_INFO_MESSAGE = "값을 입력해야 합니다."
     private const val PLAY_TIMES_INPUT_INFO_MESSAGE = "시도할 회수는 몇회인가요?"
     private const val PLAY_TIMES_ERROR_INFO_MESSAGE = "※ 숫자를 입력해주세요. ex) 5"
     private const val NAME_LENGTH_LIMIT = 5
 
     fun getCarNamesInput(): List<String> {
-        var carNameList: List<String> = ArrayList<String>()
-        while (carNameList.isEmpty()) {
+        return try {
             println(CAR_NAME_INPUT_INFO_MESSAGE)
-            carNameList = getCarNames(readLine())
+            getCarNames(readLine())
+        } catch (e: InvalidInputRacingCarException) {
+            getCarNamesInput()
         }
-        return carNameList
     }
 
     fun getTimesInput(): Int {
         println(PLAY_TIMES_INPUT_INFO_MESSAGE)
-        var times = getNumberOfTimes(readLine())
-        while (times == null) {
-            times = getNumberOfTimes(readLine())
+        return try {
+            getNumberOfTimes(readLine())
+        } catch (e: InvalidTimesInputException) {
+            getTimesInput()
         }
-        return times
-    }
-
-    private fun getNumberOfTimes(input: String?): Int? {
-        val times = try {
-            input?.toInt()
-        } catch (e: NumberFormatException) {
-            println("$PLAY_TIMES_ERROR_INFO_MESSAGE\n$PLAY_TIMES_INPUT_INFO_MESSAGE")
-            null
-        }
-        return times?.let { abs(it) }
     }
 
     private fun getCarNames(input: String?): List<String> {
+        validCarNameInputExist(input)
         val nameList = input?.split(",")
-        if (input?.isEmpty() == true || !validNameLength(nameList)) {
-            return ArrayList()
-        }
+        validCarNameLength(nameList)
         return nameList.orEmpty()
     }
 
-    private fun validNameLength(nameList: List<String>?): Boolean {
+    private fun validCarNameInputExist(input: String?) {
+        if (input?.isEmpty() == true) {
+            println(EMPTY_CAR_NAME_ERROR_INFO_MESSAGE)
+            throw EmptyCarNamesInputException(EMPTY_CAR_NAME_ERROR_INFO_MESSAGE)
+        }
+    }
+
+    private fun validCarNameLength(nameList: List<String>?) {
         val exceedNameLimitList = nameList?.filter { it.length > NAME_LENGTH_LIMIT }
         if (exceedNameLimitList?.isNotEmpty() == true) {
             println(CAR_NAME_INPUT_ERROR_INFO_MESSAGE)
-            return false
+            throw InvalidCarNamesLengthInputException(CAR_NAME_INPUT_ERROR_INFO_MESSAGE)
         }
-        return true
+    }
+
+    private fun getNumberOfTimes(input: String?): Int {
+        val times = try {
+            input?.toInt()
+        } catch (e: NumberFormatException) {
+            println(PLAY_TIMES_ERROR_INFO_MESSAGE)
+            null
+        }
+        return times ?: throw InvalidTimesInputException(PLAY_TIMES_ERROR_INFO_MESSAGE)
     }
 }
