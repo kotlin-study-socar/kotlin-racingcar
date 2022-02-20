b1d6530369a372a0220ce9767454f431e37f202d
SukWooJung <ggp03016@naver.com>
2022-02-20 12:29:01 +0900

    refactor: while 문을 통한 무한루프 제거. 재귀 방식으로 변경. check => validate 메소드 명 변경

--- src/main/kotlin/ornn/racingcar/service/InputService.kt
+++ src/main/kotlin/ornn/racingcar/service/InputService.kt
@@ -1,63 +1,61 @@
 package ornn.racingcar.service
 
-import ornn.racingcar.resource.Strings
 import ornn.racingcar.exception.IllegalInputException
+import ornn.racingcar.resource.Strings
 
-class InputService {
+object InputService {
 
     fun readCarNames(): List<String> {
-        var carNames: List<String>
-        while (true) {
-            println(Strings.ASK_CAR_NAMES)
-            val input = readLine().toString()
-            try {
-                checkInputIsNull(input)
-                carNames = input.split(",")
-                checkCarNameMoreThanFive(carNames)
-                break
-            } catch (e: IllegalInputException) {
-                println(Strings.ERROR_MESSAGE + " " + Strings.ERROR_INFO_INVALID_INPUT)
-            }
+        lateinit var carNames: List<String>
+        println(Strings.ASK_CAR_NAMES)
+        val input = readLine()
+        return try {
+            validateInputIsNull(input)
+            carNames = input!!.split(",")
+            validateCarNameMoreThanFive(carNames)
+            carNames
+        } catch (e: IllegalInputException) {
+            println(e.message)
+            readCarNames()
         }
-        return carNames
     }
 
     fun readPlayingTimes(): Int {
-        var input : String
-        while (true) {
-            println(Strings.ASK_HOW_MANY_TIMES_TO_PLAY)
-            input = readLine().toString()
-            try {
-                checkInputIsNull(input)
-                checkPlayTimeIsNum(input)
-                break
-            } catch (e: IllegalInputException) {
-                println(Strings.ERROR_MESSAGE + " " + Strings.ERROR_INFO_INVALID_INPUT)
-            }
-        }
-        return input.toInt()
-    }
-
-    fun checkPlayTimeIsNum (input : String) {
-        for (c in input) {
-            if (!c.isDigit()) {
-                throw IllegalInputException(Strings.ERROR_MESSAGE)
-            }
+        println(Strings.ASK_HOW_MANY_TIMES_TO_PLAY)
+        val input = readLine()
+        return try {
+            validateInputIsNull(input)
+            validatePlayTimeIsNum(input!!)
+            input.toInt()
+        } catch (e: IllegalInputException) {
+            println(e.message)
+            readPlayingTimes()
+        } catch (e: NumberFormatException) {
+            println(e.message)
+            readPlayingTimes()
         }
     }
 
-    fun checkCarNameMoreThanFive(carsName: List<String>) {
+    fun validatePlayTimeIsNum(input: String) {
+        try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            throw NumberFormatException(Strings.ERROR_MESSAGE + Strings.ERROR_INFO_NOT_NUM)
+        }
+    }
+
+    fun validateCarNameMoreThanFive(carsName: List<String>) {
         for (carName in carsName) {
-            if (carName.length >= 5 || carName.isEmpty()) {
-                throw  IllegalInputException(Strings.ERROR_MESSAGE);
+            if (carName.length > 5 || carName.isEmpty()) {
+                throw  IllegalInputException(Strings.ERROR_MESSAGE + Strings.ERROR_INFO_CAR_NAME_LENGTH_MORE_THAN_FIVE)
             }
         }
     }
 
-    fun checkInputIsNull(input: String?) {
-        if (input == null || input.isEmpty() || input == "null") {
-            throw IllegalInputException(Strings.ERROR_MESSAGE)
+    fun validateInputIsNull(input: String?) {
+        input?.replace(" ", "")
+        if (input == null || input.isEmpty()) {
+            throw IllegalInputException(Strings.ERROR_MESSAGE + Strings.ERROR_INFO_NULL_INPUT)
         }
     }
-
 }
